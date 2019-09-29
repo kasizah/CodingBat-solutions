97ef24c29c30d6005b274d8205409b00b322199d
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-09-29 18:54:08 -0400

    Update conCat.java

--- String-1/conCat.java
+++ String-1/conCat.java
@@ -1,1 +1,6 @@
-
+public String conCat(String a, String b) {
+  if(a.length() == 0 && b.length() == 0) return a + b;
+  else if(a.length() == 0) return b;
+  else if(b.length() == 0) return a;
+  else return (a.charAt(a.length() - 1) == b.charAt(0))? a.substring(0, a.length() - 1) + b : a + b;
+}
