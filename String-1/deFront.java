600ec31d210bcdd08b1a987ffe58774cc83aa8ee
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-09-29 21:27:46 -0400

    Update deFront.java

--- String-1/deFront.java
+++ String-1/deFront.java
@@ -1,1 +1,7 @@
-
+public String deFront(String str) {
+  if(str.length() < 2) return (str.length() == 1 && str.charAt(0) == 'a')? str : "";
+  else if(str.charAt(0) == 'a' && str.charAt(1) == 'b') return str;
+  else if(str.charAt(0) == 'a') return str.charAt(0) + str.substring(2);
+  else if(str.charAt(1) == 'b') return str.substring(1);
+  else return str.substring(2);
+}
