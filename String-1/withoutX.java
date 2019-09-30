2ef32769a084f94143dbcb645427d20a424666b4
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-09-29 21:54:44 -0400

    Update withoutX.java

--- String-1/withoutX.java
+++ String-1/withoutX.java
@@ -1,1 +1,8 @@
-
+public String withoutX(String str) {
+  if(str.length() == 0) return str;
+  else if(str.equals("x")) return "";
+  else if(str.charAt(0) == 'x' && str.charAt(str.length() - 1) == 'x') return str.substring(1, str.length() - 1);
+  else if(str.charAt(0) == 'x') return str.substring(1);
+  else if(str.charAt(str.length() - 1) == 'x') return str.substring(0, str.length() - 1);
+  else return str;
+}
