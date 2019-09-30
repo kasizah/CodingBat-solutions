de03ab0297caac7ee4fef40d156119a8053cf9da
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-09-30 10:13:16 -0400

    Update withoutX2.java

--- String-1/withoutX2.java
+++ String-1/withoutX2.java
@@ -1,1 +1,7 @@
-
+public String withoutX2(String str) {
+  if(str.length() == 0) return "";
+  else if(str.length() > 1 && str.charAt(0) == 'x' && str.charAt(1) == 'x') return str.substring(2);
+  else if(str.charAt(0) == 'x') return str.substring(1);
+  else if(str.charAt(1) == 'x') return str.charAt(0) + str.substring(2);
+  else return str;
+}
