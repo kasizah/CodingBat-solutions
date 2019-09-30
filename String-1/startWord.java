efb4e11e08f76f1efd908b94490e7d53a08a4edb
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-09-29 21:51:35 -0400

    Update startWord.java

--- String-1/startWord.java
+++ String-1/startWord.java
@@ -1,1 +1,3 @@
-
+public String startWord(String str, String word) {
+  return (str.length() >= word.length() && str.substring(1, word.length()).equals(word.substring(1)))? str.substring(0, word.length()) : "";
+}
