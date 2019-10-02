842644c572e23671a4e778f35804948e3a60d2b8
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-10-02 10:21:26 -0400

    Update makeEnds.java

--- Array-1/makeEnds.java
+++ Array-1/makeEnds.java
@@ -1,1 +1,6 @@
-
+public int[] makeEnds(int[] nums) {
+  int[] firstLast = new int[2];
+  if(nums.length == 1) firstLast[0] = firstLast[1] = nums[0];
+  else {firstLast[0] = nums[0]; firstLast[1] = nums[nums.length - 1];}
+  return firstLast;
+}
