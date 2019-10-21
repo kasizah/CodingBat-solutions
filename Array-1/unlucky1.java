163f2e044d91e7610450e7aade5ab73af8ab4f96
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-10-21 13:10:02 -0400

    Update unlucky1.java

--- Array-1/unlucky1.java
+++ Array-1/unlucky1.java
@@ -1,1 +1,10 @@
-
+public boolean unlucky1(int[] nums) {
+  boolean unlucky = false;
+  if(nums.length < 2) unlucky = false;
+  else {
+    if(nums[0] == 1 && nums[1] == 3) unlucky = true;
+    if(nums[1] == 1 && nums[2] == 3) unlucky = true;
+    if(nums[nums.length - 2] == 1 && nums[nums.length - 1] == 3) unlucky = true;
+  }
+  return unlucky;
+}
