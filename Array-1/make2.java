2590ef565381758c969524892ea6d41f9e8cb732
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-11-03 16:53:47 -0500

    Update make2.java

--- Array-1/make2.java
+++ Array-1/make2.java
@@ -1,1 +1,6 @@
-
+public int[] make2(int[] a, int[] b) {
+  int[] arrayO2 = new int[2];
+  if(a.length == 0) {arrayO2[0] = b[0]; arrayO2[1] = b[1]; return arrayO2;}
+  if(a.length == 1) {arrayO2[0] = a[0]; arrayO2[1] = b[0]; return arrayO2;}
+  else {arrayO2[0] = a[0]; arrayO2[1] = a[1]; return arrayO2;}
+}
