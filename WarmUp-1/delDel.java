19bffb99e267d7c2ba1b50f0f14a3e6a8dc35023
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-11-08 10:22:01 -0500

    Update delDel.java

--- WarmUp-1/delDel.java
+++ WarmUp-1/delDel.java
@@ -1,1 +1,5 @@
-
+public String delDel(String str) {
+  if(str.length() < 4) return str;
+  else if(str.indexOf("del") == 1) return str.charAt(0) + str.substring(str.indexOf("del") + 3);
+  else return str;
+}
