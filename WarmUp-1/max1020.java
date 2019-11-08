ab50c72f899dd4eac7bf9259dd9535bffc0972dd
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-11-08 10:49:03 -0500

    Update max1020.java

--- WarmUp-1/max1020.java
+++ WarmUp-1/max1020.java
@@ -1,1 +1,13 @@
-
+public int max1020(int a, int b) {
+  // First make it so that the bigger value is in a
+  if(b > a) {
+    int temp = a;
+    a = b;
+    b = temp;
+  }
+  
+  // Knowing a is bigger, check a first
+  if(a >= 10 && a <= 20) return a;
+  if(b >= 10 && b <= 20) return b;
+  return 0;
+}
