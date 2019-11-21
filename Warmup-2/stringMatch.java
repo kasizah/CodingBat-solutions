118593b4b64fa20550bccdcbc08f56716d7c7abf
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-11-21 16:51:50 -0500

    Update stringMatch.java

--- Warmup-2/stringMatch.java
+++ Warmup-2/stringMatch.java
@@ -1,1 +1,11 @@
-
+public int stringMatch(String a, String b) {
+  int shortest = Math.min(a.length(), b.length());
+  int count = 0;
+  
+  for(int i = 0; i < shortest - 1; i++) {
+    String aSub = a.substring(i, i + 2),
+           bSub = b.substring(i, i + 2);
+    if(aSub.equals(bSub)) count++;
+  }
+  return count;
+}
