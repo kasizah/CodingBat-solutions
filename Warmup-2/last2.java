f1a4ebb35d07d6143c05af4ede1bbed0db11bc1e
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-11-21 16:31:29 -0500

    Update last2.java

--- Warmup-2/last2.java
+++ Warmup-2/last2.java
@@ -1,1 +1,12 @@
-
+public int last2(String str) {
+  if(str.length() < 2) return 0;
+  
+  String L2 = str.substring(str.length() - 2);
+  int count = 0;
+  
+  for(int i = 0; i < str.length() - 2; i++) {
+    String sub = str.substring(i, i + 2);
+    if(sub.equals(L2)) count++;
+  }
+  return count;
+}
