bbf1000de6b1ddb6b50b13c2bd58af58cbc90b5b
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-11-21 16:42:30 -0500

    Update arrayFront9.java

--- Warmup-2/arrayFront9.java
+++ Warmup-2/arrayFront9.java
@@ -1,1 +1,9 @@
-
+public boolean arrayFront9(int[] nums) {
+  int end = nums.length;
+  end = end > 4? 4 : end;
+  
+  for(int i = 0; i < end; i++) {
+    if(nums[i] == 9) return true;
+  }
+  return false;
+}
