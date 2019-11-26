cf4871700c98ce66f9b53d3dae320dc0e832311d
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2019-11-26 13:09:03 -0600

    Update altPairs.java

--- Warmup-2/altPairs.java
+++ Warmup-2/altPairs.java
@@ -1,1 +1,7 @@
-
+public String altPairs(String str) {
+  String result = "";
+  for(int i = 0; i < str.length(); i += 4) {
+    result += str.charAt(i) + "" + ((i + 1 > str.length() - 1)? "" : str.charAt(i + 1));
+  }
+  return result;
+}
