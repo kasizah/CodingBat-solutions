093860ce3dda626ad961d34d376d27e67fe3a883
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2020-01-06 12:29:08 -0500

    Update noTriples.java

--- Warmup-2/noTriples.java
+++ Warmup-2/noTriples.java
@@ -1,1 +1,12 @@
-
+// CodingBat solution
+public boolean noTriples(int[] nums) {
+  // Iterate < length-2, so can use i+1 in the loop.
+  // Return false immediately if upon seeing a triple.
+  for(int i = 0; i < (nums.length-2); i++) {
+    int first = nums[i];
+    if(nums[i+1]==first && nums[i+2]==first) return false;
+  }
+  
+  // If we get here ... no triples.
+  return true;
+}
