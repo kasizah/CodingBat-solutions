7d9cc77f52af2e532e05f6693ca36bcd650ef796
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2020-01-22 12:45:51 -0500

    Update answerCell.java

--- Logic-1/answerCell.java
+++ Logic-1/answerCell.java
@@ -1,1 +1,7 @@
-
+public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+  if(isAsleep) return false;
+  else {
+    if(isMorning) return isMom? true : false;
+    else return true;
+  }
+}
