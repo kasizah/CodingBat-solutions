a2342cf6e442d06fec38795b9a2e79e54d2cfda0
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2020-04-23 11:14:22 -0500

    Update greenTicket.java

--- Logic-1/greenTicket.java
+++ Logic-1/greenTicket.java
@@ -1,1 +1,7 @@
-
+public int greenTicket(int a, int b, int c) {
+  int lotto = 0;
+  if(a != b && b != c && c != a) lotto = 0;
+  if(a == b || b == c || c == a) lotto = 10;
+  if(a == b && b == c) lotto = 20;
+  return lotto;
+}
