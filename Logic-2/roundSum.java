67166ee4d6c554367fb3004fb53e312d94c50a38
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2020-04-26 16:24:06 -0500

    Update roundSum.java

--- Logic-2/roundSum.java
+++ Logic-2/roundSum.java
@@ -1,1 +1,9 @@
+public int roundSum(int a, int b, int c) {
+  return round10(a) + round10(b) + round10(c);
+}
 
+public int round10(int num) {
+  int roundDown = (num / 10) * 10;
+  int roundUp = ((num / 10)+1)*10;
+  return (num % 10 >= 5)? roundUp : roundDown;
+}
