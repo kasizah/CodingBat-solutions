17eed5cae47138b0c93066c1d245eb0048b8fecf
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2020-04-26 17:17:56 -0500

    Update closeFar.java

--- Logic-2/closeFar.java
+++ Logic-2/closeFar.java
@@ -1,1 +1,12 @@
+public boolean closeFar(int a, int b, int c) {
+  if(Math.abs(b - c) < 2) return false;
+  return (close(a, b) && far(a, c)) || (close(a, c) && far(a, c));
+}
 
+public boolean close(int x, int n) {
+  return (Math.abs(x - n) <= 1)? true : false;
+}
+
+public boolean far(int x, int n) {
+  return (Math.abs(x - n)? >= 2)? true : false;
+}
