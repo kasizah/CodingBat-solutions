92d8cb1ff5ff9e02ff2d0d78f68e46987abdce4f
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2020-04-26 16:14:44 -0500

    Update noTeenSum.java

--- Logic-2/noTeenSum.java
+++ Logic-2/noTeenSum.java
@@ -1,1 +1,9 @@
+public int noTeenSum(int a, int b, int c) {
+  return fixTeen(a) + fixTeen(b) + fixTeen(c);
+}
 
+public int fixTeen(int n) {
+  if(n >= 13 && n <= 19) {
+    return (n == 15 || n == 16)? n : 0;
+  } return n;
+}
