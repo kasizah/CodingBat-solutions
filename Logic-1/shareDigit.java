306030a38578d1caa364668bda3c7fd4ad6e4870
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2020-04-27 11:04:01 -0500

    Update shareDigit.java

--- Logic-1/shareDigit.java
+++ Logic-1/shareDigit.java
@@ -1,6 +1,9 @@
 public boolean shareDigit(int a, int b) {
   int fd1 = a / 10, sd1 = a % 10;
   int fd2 = b / 10, sd2 = b % 10;
-  if(fd1 == fd2 || fd1 == sd2 || sd1 == fd2 || sd1 == sd2) return true;
+  if(fd1 == fd2) return true;
+  if(fd1 == sd2) return true;
+  if(sd1 == fd2) return true;
+  if(sd1 == sd2) return true;
   return false;
 }
