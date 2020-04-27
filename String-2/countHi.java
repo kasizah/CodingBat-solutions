59f7488dfdfbc8702dab3fa6ab614e8705b9b9b9
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2020-04-27 12:24:20 -0500

    Update countHi.java

--- String-2/countHi.java
+++ String-2/countHi.java
@@ -1,1 +1,7 @@
-
+public int countHi(String str) {
+  int countHi = 0;
+  for(int i = 0; i < str.length() - 1; i++) {
+    if(str.substring(i, i + 2).equals("hi")) countHi++;
+  }
+  return countHi;
+}
