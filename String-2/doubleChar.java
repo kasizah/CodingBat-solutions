7b5f8b19bf983992d51b9162898342968614adbd
Kris Caruso (K.M.C.) <cchristophercmc@gmail.com>
2020-04-27 12:19:03 -0500

    Update doubleChar.java

--- String-2/doubleChar.java
+++ String-2/doubleChar.java
@@ -1,1 +1,6 @@
-
+public String doubleChar(String str) {
+  String doubled = "";
+  for(int i = 0; i < str.length(); i++) {
+    doubled += str.substring(i, i + 1) + str.substring(i, i + 1);
+  } return doubled;
+}
