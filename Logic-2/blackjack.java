40282c38f10bd8d957d3b32db4af1a013aee521b
Ka$izah <cchristophercmc@gmail.com>
2021-11-26 17:07:57 -0600

    Update blackjack.java

--- Logic-2/blackjack.java
+++ Logic-2/blackjack.java
@@ -1,1 +1,17 @@
-
+public int blackjack(int a, int b) {
+  if(a > 21 && b > 21) return 0;
+  /**
+   * one value is less than 21
+   * one value is more than 21
+   */
+  if(a > 21) return b;
+  if(b > 21) return a;
+  /**
+   * both values are less than
+   * or equal to 21
+   */
+  int winner = 0;
+  if(a > b && a <= 21) winner = a;
+  if(b > a && b <= 21) winner = b;
+  return winner;
+}
