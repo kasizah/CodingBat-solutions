2515a4967619bb7d55b44f5c33d976df6ebc15fd
Ka$izah <cchristophercmc@gmail.com>
2021-11-27 14:31:04 -0600

    Update xyBalance.java

--- String-2/xyBalance.java
+++ String-2/xyBalance.java
@@ -1,1 +1,18 @@
-
+public boolean xyBalance(String str) {
+  boolean x = false, y = false;
+  
+  for(int i = 0; i < str.length(); i++) {
+    if(str.charAt(i) == 'x' && y == true) {
+      x = true;
+      y = true;
+    } else if(str.charAt(i) == 'x') {
+      x = true;
+    }
+    if(str.charAt(i) == 'y' && x == true) {
+      y = true;
+    }
+  }
+  if(x == false)
+    y = true;
+  return y;
+}
