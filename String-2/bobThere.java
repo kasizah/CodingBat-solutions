0cf582127c8e966fc9d721199e324e568aabb5fe
Ka$izah <cchristophercmc@gmail.com>
2021-11-27 14:25:19 -0600

    Update bobThere.java

--- String-2/bobThere.java
+++ String-2/bobThere.java
@@ -1,1 +1,6 @@
-
+public boolean bobThere(String str) {
+  for(int i = 0; i < str.length() - 2; i++) {
+    if(str.charAt(i) == str.charAt(i + 2) && str.charAt(i + 2) == 'b') return true;
+  }
+  return false;
+}
