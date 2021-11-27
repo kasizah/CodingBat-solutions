dd75f7cd87ddf571bdb84dfaac6d4ac36c988ccd
Ka$izah <cchristophercmc@gmail.com>
2021-11-27 13:13:44 -0600

    Update makeChocolate.java

--- Logic-2/makeChocolate.java
+++ Logic-2/makeChocolate.java
@@ -1,1 +1,25 @@
-
+// PREVIOUS SOLUTION -> DOESN'T WORK
+/**
+ *  public int makeChocolate(int small, int big, int goal) {
+ *    int remainder = goal % 5;
+ *    
+ *    if(small + (big * 5) < goal) {
+ *      return -1;
+ *    } else if(remainder <= small && goal - (big * 5) > 4) {
+ *      return remainder + 5;
+ *    } else if(remainder <= small) {
+ *      return remainder;
+ *    } else {
+ *      return -1;
+ *    }
+ *  }
+ */
+public int makeChocolate(int small, int big, int goal) {
+  if(goal >= 5) {
+    if(goal/5 >= big)
+      goal -= big * 5;
+    else
+      goal %= 5;
+  } else if(goal <= small) return goal;
+  else return -1;
+}
