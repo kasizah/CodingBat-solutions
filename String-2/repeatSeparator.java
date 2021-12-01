8c9890fddfcf920a4fb99ebf80a5d2f200a8ba5a
Ka$izah <cchristophercmc@gmail.com>
2021-11-30 20:38:51 -0500

    Update repeatSeparator.java

--- String-2/repeatSeparator.java
+++ String-2/repeatSeparator.java
@@ -1,1 +1,10 @@
-
+public String repeatSeparator(String word, String sep, int count) {
+  if(count == 0) return "";
+  if(count == 1) return word;
+  
+  String repeated = "";
+  for(int i = 0; i < count - 1; i++) {
+    repeated += word + sep;
+  }
+  return repeated + word;
+}
