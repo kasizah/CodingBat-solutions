edd58ca461d7b43f2b5082c24b2b7133a1edb575
Ka$izah <cchristophercmc@gmail.com>
2021-11-30 20:05:41 -0500

    Update mixString.java

--- String-2/mixString.java
+++ String-2/mixString.java
@@ -1,1 +1,17 @@
-
+public String mixString(String a, String b) {
+  String mix = "", smaller, a2 = a, b2 = b;
+  
+  if(a.length() != b.length())
+    smaller = (a.length() < b.length())? a : b;
+  else
+    smaller = a;
+  
+  for(int i = 0; i < smaller.length(); i++) {
+    mix += a.substring(i, i + 1) + b.substring(i, i + 1);
+    
+    a2 = (i + 1 < a.length())? a.substring(i + 1) : "";
+    b2 = (i + 1 < b.length())? b.substring(i + 1) : "";
+  }
+  
+  return mix + a2 + b2;
+}
