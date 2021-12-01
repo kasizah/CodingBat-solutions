aab87088a5247de0432caf6cf642054c5b02f76f
Ka$izah <cchristophercmc@gmail.com>
2021-11-30 20:34:52 -0500

    Update repeatFront.java

--- String-2/repeatFront.java
+++ String-2/repeatFront.java
@@ -1,1 +1,11 @@
-
+public String repeatFront(String str, int n) {
+  if(n > str.length()) return "";
+  
+  String fs = str.substring(0, n);
+  
+  String repeat = "";
+  for(int i = 0; i < n; i++) {
+    repeat += fs; fs = fs.substring(0, fs.length() - 1);
+  }
+  return repeat;
+}
