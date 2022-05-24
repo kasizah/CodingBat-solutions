c4463681fd773a4f39753e73f0d5e014ac9d74d3
Kasizah <cchristophercmc@gmail.com>
2022-05-24 15:58:10 -0400

    Update starOut.java

--- String-2/starOut.java
+++ String-2/starOut.java
@@ -1,1 +1,23 @@
-
+public String starOut(String str) {
+  if(str.length() < 1) return "";
+  if(str.length() == 1) {
+    return (str.charAt(0) == '*')? "" : str;
+  }
+  char[] arr = new char[str.length()];
+  int index = 0;
+  
+  if(str.charAt(0) != '*' && str.charAt(1) != '*') {
+    arr[index] = str.charAt(0); index++;
+  }
+  for(int i = 1; i < str.length() - 1; i++) {
+    if(str.charAt(i - 1) != '*' && str.charAt(i) != '*' && str.charAt(i + 1) != '*') {
+      arr[index] = str.charAt(i);
+      index++;
+    }
+  }
+  if(str.charAt(str.length() - 2) != '*' && str.charAt(str.length() - 1) != '*') {
+    arr[index] = str.charAt(str.length() - 1);
+    index++;
+  }
+  return new String(arr, 0, index);
+}
