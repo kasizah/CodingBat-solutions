3b66f156761a48786b4c3c5ef4ecf34897770ace
Kasizah <cchristophercmc@gmail.com>
2022-05-24 16:03:12 -0400

    Update oneTwo.java

--- String-2/oneTwo.java
+++ String-2/oneTwo.java
@@ -1,1 +1,8 @@
-
+public String oneTwo(String str) {
+  String strNew = "";
+  for(int i = 0; i < str.length() - 3; i += 3) {
+    String three = str.substring(i, i + 3);
+    strNew += three.substring(1, 3) + three.substring(0, 1);
+  }
+  return strNew;
+}
