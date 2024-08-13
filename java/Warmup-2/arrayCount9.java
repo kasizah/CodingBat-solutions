388f64a16e5a32b057496a07cc5ff0646d2dbb12
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 13:18:59 -0400

    Update arrayCount9.java

--- java/Warmup-2/arrayCount9.java
+++ java/Warmup-2/arrayCount9.java
@@ -1,7 +1,7 @@
 public int arrayCount9(int[] nums) {
-  int count = 0;
-  for(int i = 0; i < nums.length; i++) {
-    count += nums[i] == 9? 1 : 0;
-  }
-  return count;
+    int count = 0;
+    for(int i = 0; i < nums.length; i++) {
+        count += nums[i] == 9? 1 : 0;
+    }
+    return count;
 }
