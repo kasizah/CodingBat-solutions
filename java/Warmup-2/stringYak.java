63a52528f72375d78fb1c105ff3d39603f156d0e
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 13:24:33 -0400

    Update stringYak.java

--- java/Warmup-2/stringYak.java
+++ java/Warmup-2/stringYak.java
@@ -1,11 +1,11 @@
 public String stringYak(String str) {
-  String result = "";
-  for(int i = 0; i < str.length(); i++) {
-    if(i + 2 < str.length() && str.charAt(i) == 'y' && str.charAt(i+2) == 'k') {
-      i += 2;
-    } else {
-      result += str.charAt(i);
+    String result = "";
+    for(int i = 0; i < str.length(); i++) {
+        if(i + 2 < str.length() && str.charAt(i) == 'y' && str.charAt(i+2) == 'k') {
+            i += 2;
+        } else {
+            result += str.charAt(i);
+        }
     }
-  }
-  return result;
+    return result;
 }
