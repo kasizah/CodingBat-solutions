2239817b0ce502276fda9f389ade2a808ea23b02
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 13:24:06 -0400

    Update stringX.java

--- java/Warmup-2/stringX.java
+++ java/Warmup-2/stringX.java
@@ -1,9 +1,9 @@
 public String stringX(String str) {
-  String result = "";
-  for(int i = 0; i < str.length(); i++) {
-    if(!(i > 0 && i < str.length()-1 && str.charAt(i) == 'x')) {
-      result += str.charAt(i);
+    String result = "";
+    for(int i = 0; i < str.length(); i++) {
+        if(!(i > 0 && i < str.length()-1 && str.charAt(i) == 'x')) {
+            result += str.charAt(i);
+        }
     }
-  }
-  return result;
+    return result;
 }
