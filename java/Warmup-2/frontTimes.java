e05ac34f66b2e5a62cbbf941cdf07eeed1fc04dd
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 13:22:17 -0400

    Update frontTimes.java

--- java/Warmup-2/frontTimes.java
+++ java/Warmup-2/frontTimes.java
@@ -1,12 +1,12 @@
 public String frontTimes(String str, int n) {
-  String result = "", front3 = "";
-  if(str.length() >= 3) {
-    front3 = str.substring(0, 3);
-  } else {
-    front3 = str;
-  }
-  for(int i = 0; i < n; i++) {
-    result += front3;
-  }
-  return result;
+    String result = "", front3 = "";
+    if(str.length() >= 3) {
+        front3 = str.substring(0, 3);
+    } else {
+        front3 = str;
+    }
+    for(int i = 0; i < n; i++) {
+        result += front3;
+    }
+    return result;
 }
