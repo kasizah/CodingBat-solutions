d86ad7aefe5c2c65109edf58b8a961613095eca8
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 13:23:33 -0400

    Update stringSplosion.java

--- java/Warmup-2/stringSplosion.java
+++ java/Warmup-2/stringSplosion.java
@@ -1,7 +1,7 @@
 public String stringSplosion(String str) { 
-  String result = "";
-  for(int i = 0; i < str.length(); i++) {
-    result += str.substring(0, i+1);
-  }
-  return result;
+    String result = "";
+    for(int i = 0; i < str.length(); i++) {
+        result += str.substring(0, i+1);
+    }
+    return result;
 }
