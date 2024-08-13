bce12371626eaf91cdf194f46f3ed28768abaa2a
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 13:22:53 -0400

    Update stringBits.java

--- java/Warmup-2/stringBits.java
+++ java/Warmup-2/stringBits.java
@@ -1,7 +1,7 @@
 public String stringBits(String str) {
-  String result = "";
-  for(int i = 0; i < str.length(); i+=2) {
-    result += str.charAt(i);
-  }
-  return result;
+    String result = "";
+    for(int i = 0; i < str.length(); i+=2) {
+        result += str.charAt(i);
+    }
+    return result;
 }
