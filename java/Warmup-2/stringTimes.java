4f445d3e80f0ef6bd44b9501e3fe7c8ad6356a22
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 13:23:48 -0400

    Update stringTimes.java

--- java/Warmup-2/stringTimes.java
+++ java/Warmup-2/stringTimes.java
@@ -1,7 +1,7 @@
 public String stringTimes(String str, int n) {
-  String result = "";
-  for(int i = 1; i <= n; i++) {
-    result += str;
-  }
-  return result;
+    String result = "";
+    for(int i = 1; i <= n; i++) {
+        result += str;
+    }
+    return result;
 }
