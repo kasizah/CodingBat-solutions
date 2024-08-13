a186872761b70eddd67cd2822aa886fff718c552
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 09:55:23 -0400

    Delete Array-1/makeEnds.java

--- Array-1/makeEnds.java
+++
@@ -1,6 +0,0 @@
-public int[] makeEnds(int[] nums) {
-  int[] firstLast = new int[2];
-  if(nums.length == 1) firstLast[0] = firstLast[1] = nums[0];
-  else {firstLast[0] = nums[0]; firstLast[1] = nums[nums.length - 1];}
-  return firstLast;
-}
