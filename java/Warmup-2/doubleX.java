8d3190bf37cf12dc2725149d788c4b6b0d3edd40
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 12:53:51 -0400

    Update doubleX.java

--- java/Warmup-2/doubleX.java
+++ java/Warmup-2/doubleX.java
@@ -1,3 +1,5 @@
 boolean doubleX(String str) {
-  return str.length() >= 2 && (str.substring(0, 2).equals("xx") || str.substring(1, 3).equals("xx"));
+  int i = str.indexOf("x");
+  if(i == -1 || i+1 >= str.length()) return false;
+  return str.substring(i+1, i+2).equals("x");
 }
