ceb3398d38fe2c122be42b74219f2975fc767b8a
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 13:54:02 -0400

    Create array123.py

---
+++ python/Warmup-2/array123.py
@@ -0,0 +1,8 @@
+def array123(nums):
+    if len(nums) < 3:
+        return False
+    else:
+        for i in range(len(nums)-2):
+            if nums[i] == 1 and nums[i+1] == 2 and nums[i+2] == 3:
+                return True
+        return False
