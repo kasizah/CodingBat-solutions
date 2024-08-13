d9943f96f5f2188838f0cec7497b07b1b6304d02
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 13:25:48 -0400

    Update backAround.java

--- java/WarmUp-1/backAround.java
+++ java/WarmUp-1/backAround.java
@@ -1,3 +1,3 @@
 public String backAround(String str) {
-  return str.charAt(str.length() - 1) + str + str.charAt(str.length() - 1);
+    return str.charAt(str.length() - 1) + str + str.charAt(str.length() - 1);
 }
