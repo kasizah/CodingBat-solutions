5946ac60ff44f94ce03bae2e22cd743925d29981
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 12:58:26 -0400

    Update last2.java

--- java/Warmup-2/last2.java
+++ java/Warmup-2/last2.java
@@ -1,12 +1,11 @@
 public int last2(String str) {
   if(str.length() < 2) return 0;
   
-  String L2 = str.substring(str.length() - 2);
   int count = 0;
-  
-  for(int i = 0; i < str.length() - 2; i++) {
-    String sub = str.substring(i, i + 2);
-    if(sub.equals(L2)) count++;
+  String end = str.substring(str.length()-2);
+
+  for(int i = 0; i < str.length()-2; i++) {
+    if(str.substring(i, i+2).equals(end)) count++;
   }
   return count;
 }
