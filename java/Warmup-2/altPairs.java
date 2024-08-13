d55323b18f2e8dc91da75192185ee987d4374403
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 13:07:53 -0400

    Update altPairs.java

--- java/Warmup-2/altPairs.java
+++ java/Warmup-2/altPairs.java
@@ -1,7 +1,9 @@
 public String altPairs(String str) {
   String result = "";
   for(int i = 0; i < str.length(); i += 4) {
-    result += str.charAt(i) + "" + ((i + 1 > str.length() - 1)? "" : str.charAt(i + 1));
+    int end = i + 2;
+    if(end > str.length()) end = str.length();
+    result += str.substring(i, end);
   }
   return result;
 }
