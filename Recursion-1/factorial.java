207422e46354c5f2712ad7ab37c50463cbe8e22c
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 10:04:40 -0400

    Delete Recursion-1 directory

--- Recursion-1/factorial.java
+++
@@ -1,3 +0,0 @@
-public int factorial(int n) {
-  return (n > 1)? n * factorial(n - 1) : 1;
-}
--- Recursion-1/fibonacci.java
+++
@@ -1,4 +0,0 @@
-public int fibonacci(int n) {
-  if(n <= 1) return n;
-  return fibonacci(n - 2) + fibonacci(n - 1);
-}
