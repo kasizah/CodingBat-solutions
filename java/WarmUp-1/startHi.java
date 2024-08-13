d185208c3b70f07500d0a06aa09d5247718e34a3
Karis Caruso <cchristophercmc@gmail.com>
2024-08-13 10:24:04 -0400

    Update startHi.java

--- java/WarmUp-1/startHi.java
+++ java/WarmUp-1/startHi.java
@@ -1,3 +1,3 @@
 public boolean startHi(String str) {
-  return (str.length() >= 2 && str.substring(0, 2).equals("hi"))? true : false;
+  return str.length() >= 2 && str.substring(0, 2).equals("hi");
 }
