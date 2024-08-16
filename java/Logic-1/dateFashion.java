2f4b46653df84964e041a688aa251f6558840443
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 17:13:12 -0400

    Update dateFashion.java

--- java/Logic-1/dateFashion.java
+++ java/Logic-1/dateFashion.java
@@ -1,3 +1,6 @@
 public int dateFashion(int you, int date) {
-  return (you >= 8 && date > 2)||(you > 2 && date >= 8)? 2 : ((you <= 2 || date <= 2)? 0 : 1);
+    return (you >= 8 && date > 2) || (you > 2 && date >= 8)? 
+        2 : (
+            (you <= 2 || date <= 2)? 0 : 1
+        );
 }
