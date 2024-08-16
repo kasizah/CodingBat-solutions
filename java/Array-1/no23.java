b02eba294ba94456e2c0da27790f70615a9bd77b
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 11:46:21 -0400

    Update no23.java

--- java/Array-1/no23.java
+++ java/Array-1/no23.java
@@ -1,3 +1,4 @@
 public boolean no23(int[] nums) {
-  return nums[0] != 2 && nums[1] != 2 && nums[0] != 3 && nums[1] != 3;
+    return (nums[0] != 2 && nums[1] != 2) 
+        && (nums[0] != 3 && nums[1] != 3);
 }
