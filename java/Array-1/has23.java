836a68267fc60f7a7aed37daecd5691ffd218655
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 11:26:21 -0400

    Update has23.java

--- java/Array-1/has23.java
+++ java/Array-1/has23.java
@@ -1,3 +1,4 @@
 public boolean has23(int[] nums) {
-  return nums[0] == 2 || nums[1] == 2 || nums[0] == 3 || nums[1] == 3;
+    return (nums[0] == 2 || nums[1] == 2)
+        || (nums[0] == 3 || nums[1] == 3);
 }
