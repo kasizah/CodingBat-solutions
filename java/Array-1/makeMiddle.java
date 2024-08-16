4f297e41a941d0fa21c2e22255932e31e7f599c0
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 11:39:41 -0400

    Update makeMiddle.java

--- java/Array-1/makeMiddle.java
+++ java/Array-1/makeMiddle.java
@@ -1,4 +1,7 @@
 public int[] makeMiddle(int[] nums) {
-  int[] middle = {nums[nums.length/2 - 1], nums[nums.length/2]};
-  return middle;
+    int[] middle = {
+        nums[nums.length/2 - 1], 
+        nums[nums.length/2]
+    };
+    return middle;
 }
