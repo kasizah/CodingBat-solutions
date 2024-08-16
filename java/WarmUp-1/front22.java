658f4af360b327b25b2336fa6137e102a1bf7c48
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 16:52:38 -0400

    Update front22.java

--- java/WarmUp-1/front22.java
+++ java/WarmUp-1/front22.java
@@ -1,4 +1,4 @@
 public String front22(String str) {
-  if(str.length() < 2) return str + str + str;
-  else return str.substring(0, 2) + str + str.substring(0, 2);
+    if(str.length() < 2) return str + str + str;
+    else return str.substring(0, 2) + str + str.substring(0, 2);
 }
