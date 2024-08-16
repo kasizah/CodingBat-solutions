8c7c777067c2083db76c8db39cbf8baba12d8a0f
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 11:55:45 -0400

    Update swapEnds.java

--- java/Array-1/swapEnds.java
+++ java/Array-1/swapEnds.java
@@ -1,5 +1,8 @@
 public int[] swapEnds(int[] nums) {
-  int first = nums[0], last = nums[nums.length-1];
-  nums[0] = last; nums[nums.length-1] = first;
-  return nums;
+    if(nums.length > 1) {
+        int temp = nums[0];
+        nums[0] = nums[nums.length-1];
+        nums[nums.length-1] = temp;
+    }
+    return nums;
 }
