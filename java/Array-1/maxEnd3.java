6f406937e9bfdd643c1b8545e73c600210f40893
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 11:42:00 -0400

    Update maxEnd3.java

--- java/Array-1/maxEnd3.java
+++ java/Array-1/maxEnd3.java
@@ -1,5 +1,7 @@
 public int[] maxEnd3(int[] nums) {
-  int max = (nums[2] > nums[0])? nums[2] : nums[0];
-  for(int i = 0; i < nums.length; i++) nums[i] = max;
-  return nums;
+    int max = (nums[2] > nums[0])? nums[2] : nums[0];
+    for(int i = 0; i < nums.length; i++) {
+        nums[i] = max;
+    }
+    return nums;
 }
