401f6dbf60428ead2b4062c454954557483d303d
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 17:16:12 -0400

    Update alarmClock.java

--- java/Logic-1/alarmClock.java
+++ java/Logic-1/alarmClock.java
@@ -1,4 +1,6 @@
 public String alarmClock(int day, boolean vacation) {
-  if(vacation) return (day >= 1 && day <= 5)? "10:00" : "off";
-  else return (day >= 1 && day <= 5)? "7:00" : "10:00";
+    if(vacation) {
+        return (day >= 1 && day <= 5)? "10:00" : "off";
+    } 
+    return (day >= 1 && day <= 5)? "7:00" : "10:00";
 }
