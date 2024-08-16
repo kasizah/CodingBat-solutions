10ee5f91fc05ef54dad8fe59315a6ed0f80d1105
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 11:44:45 -0400

    Update midThree.java

--- java/Array-1/midThree.java
+++ java/Array-1/midThree.java
@@ -1,4 +1,8 @@
 public int[] midThree(int[] nums) {
-  int[] middle = {nums[nums.length/2 - 1], nums[nums.length/2], nums[nums.length/2 + 1]};
-  return middle;
+    int[] middle = {
+        nums[nums.length/2 - 1], 
+        nums[nums.length/2], 
+        nums[nums.length/2 + 1]
+    };
+    return middle;
 }
