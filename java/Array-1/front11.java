01e6a0bd78d8ef85713fc4f3baeaa60f34ab29b9
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 11:23:31 -0400

    Update front11.java

--- java/Array-1/front11.java
+++ java/Array-1/front11.java
@@ -1,11 +1,18 @@
 public int[] front11(int[] a, int[] b) {
-  int[] a0b0;
-  if(a.length == 0 && b.length == 0) a0b0 = new int[0];
-  else if(a.length == 0 || b.length == 0){
-    a0b0 = new int[1];
-    if(a.length == 0 && b.length != 0) a0b0[0] = b[0];
-    if(a.length != 0 && b.length == 0) a0b0[0] = a[0];
-  }
-  else {a0b0 = new int[2]; a0b0[0] = a[0]; a0b0[1] = b[0];}
-  return a0b0;
+    int[] nums;
+    if(a.length == 0 && b.length == 0) {
+        nums = new int[0];
+    } else if(Math.min(a.length, b.length) == 0) {
+        nums = new int[1];
+        if(a.length == 0) {
+            nums[0] = b[0];
+        } else {
+            nums[0] = a[0];
+        }
+    } else {
+        nums = new int[2]; 
+        nums[0] = a[0]; 
+        nums[1] = b[0];
+    }
+    return nums;
 }
