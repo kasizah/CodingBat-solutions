54d94fb9bb06f46116259d7c21e2fc9f9ea6aa0d
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 11:18:47 -0400

    Update double23.java

--- java/Array-1/double23.java
+++ java/Array-1/double23.java
@@ -1,6 +1,6 @@
 public boolean double23(int[] nums) {
-  for(int i = 0; i < nums.length - 1; i++) {
-    if(nums[i] == nums[i + 1] && (nums[i + 1] == 2 || nums[i + 1] == 3)) return true;
-  }
-  return false;
+    for(int i = 0; i < nums.length - 1; i++) {
+        if(nums[i] == nums[i + 1] && (nums[i + 1] == 2 || nums[i + 1] == 3)) return true;
+    }
+    return false;
 }
