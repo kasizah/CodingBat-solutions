16a32b3adfd49542bb83b8cec24c4a8d20318ddf
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 11:43:47 -0400

    Update maxTriple.java

--- java/Array-1/maxTriple.java
+++ java/Array-1/maxTriple.java
@@ -1,6 +1,10 @@
 public int maxTriple(int[] nums) {
-  int a = nums[0], b = nums[nums.length/2], c = nums[nums.length - 1];
-  int max = (a > b || a == b)? a : b;
-  max = (max > c || max == c)? max : c;
-  return max;
+    int a = nums[0], 
+        b = nums[nums.length/2], 
+        c = nums[nums.length-1];
+    
+    int max = (a > b || a == b)? a : b;
+    max = (max > c || max == c)? max : c;
+    
+    return max;
 }
