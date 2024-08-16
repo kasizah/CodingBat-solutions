2cd7978bd455ef3698a918c7eb2b4b60d7681c06
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 17:00:06 -0400

    Update mixStart.java

--- java/WarmUp-1/mixStart.java
+++ java/WarmUp-1/mixStart.java
@@ -1,5 +1,6 @@
 public boolean mixStart(String str) {
-  if(str.length() >= 3) {
-    return str.substring(1, 3).equals("ix");
-  } else return false;
+    if(str.length() >= 3) {
+        return str.substring(1, 3).equals("ix");
+    }
+    return false;
 }
