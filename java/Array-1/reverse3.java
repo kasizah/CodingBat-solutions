9207ad53db5a30920b88122e7c81fd2475d0ca6f
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 11:48:47 -0400

    Update reverse3.java

--- java/Array-1/reverse3.java
+++ java/Array-1/reverse3.java
@@ -1,7 +1,7 @@
 public int[] reverse3(int[] nums) {
-  int[] reversed = new int[3];
-  reversed[0] = nums[2];
-  reversed[1] = nums[1];
-  reversed[2] = nums[0];
-  return reversed;
+    int[] reversed = new int[nums.length];
+    for(int i = nums.length-1; i >= 0; i--) {
+        reversed[i] = nums[nums.length-1-i];
+    }
+    return reversed;
 }
