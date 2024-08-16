00fe4916cbfa53dc17a3c5abc908dd136f1af2ec
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 11:32:17 -0400

    Update makeEnds.java

--- java/Array-1/makeEnds.java
+++ java/Array-1/makeEnds.java
@@ -1,6 +1,7 @@
 public int[] makeEnds(int[] nums) {
-  int[] firstLast = new int[2];
-  if(nums.length == 1) firstLast[0] = firstLast[1] = nums[0];
-  else {firstLast[0] = nums[0]; firstLast[1] = nums[nums.length - 1];}
-  return firstLast;
+    int[] ends = {nums[0], nums[0]};
+    if(nums.length > 1) {
+        ends[1] = nums[nums.length-1];
+    }
+    return ends;
 }
