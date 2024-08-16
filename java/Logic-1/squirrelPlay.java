fe5cca77eea1678fb4fc20c69a457f8871d8d8a3
Karis Caruso <cchristophercmc@gmail.com>
2024-08-16 17:14:22 -0400

    Update squirrelPlay.java

--- java/Logic-1/squirrelPlay.java
+++ java/Logic-1/squirrelPlay.java
@@ -1,4 +1,6 @@
 public boolean squirrelPlay(int temp, boolean isSummer) {
-  if(isSummer) return (temp >= 60 && temp <= 100)? true : false;
-  return (temp >= 60 && temp <= 90)? true : false;
+    if(isSummer) {
+        return temp >= 60 && temp <= 100;
+    }
+    return temp >= 60 && temp <= 90;
 }
