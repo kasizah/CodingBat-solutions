3708e1fa0c22c81612870b76dc37d386667127fa
Karis Caruso <cchristophercmc@gmail.com>
2024-08-20 17:13:16 -0400

    Update answerCell.java

--- java/Logic-1/answerCell.java
+++ java/Logic-1/answerCell.java
@@ -1,7 +1,11 @@
 public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
-  if(isAsleep) return false;
-  else {
-    if(isMorning) return isMom? true : false;
-    else return true;
-  }
+    if(isAsleep) return false;
+    else {
+        if(isMorning) 
+            return isMom;
+        /* default answer:
+         *   not morning, not mom, not asleep
+        */
+        return true;
+    }
 }
