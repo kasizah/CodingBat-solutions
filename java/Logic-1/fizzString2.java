c638fb6c1de9c093d06b41e2d697baaec4a6f3af
Karis Caruso <cchristophercmc@gmail.com>
2024-08-22 17:20:31 -0400

    Update fizzString2.java

--- java/Logic-1/fizzString2.java
+++ java/Logic-1/fizzString2.java
@@ -1,7 +1,9 @@
 public String fizzString2(int n) {
-  int x = n % 3, y = n % 5;
-  if(n % 3 == 0 && n & 5 == 0) return "FizzBuzz!";
-  else if(n % 3 == 0 || x % 3 == 0) return "Fizz!";
-  else if(n % 5 == 0 || y % 5 == 0) return "Buzz!";
-  else return n + "!";
+    String ret = "";
+    ret += (n % 3 == 0)? "Fizz" : "";
+    ret += (n % 5 == 0)? "Buzz" : "";
+    if(ret.equals("")) {
+        return n + "!";
+    }
+    return ret + "!";
 }
