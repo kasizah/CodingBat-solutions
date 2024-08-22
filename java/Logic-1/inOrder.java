fd9fba933c9caa12bec1a97f23a9e5a791d222e2
Karis Caruso <cchristophercmc@gmail.com>
2024-08-22 17:29:00 -0400

    Update inOrder.java

--- java/Logic-1/inOrder.java
+++ java/Logic-1/inOrder.java
@@ -1,4 +1,6 @@
 public boolean inOrder(int a, int b, int c, boolean bOk) {
-  if(bOk) return c > b;
-  else return a < b && b < c;
+    if(bOk) {
+        return c > b;
+    }
+    return a < b && b < c;
 }
