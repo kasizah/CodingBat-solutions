c748f663ccf5435b5cbf4f09570e3915a3d7a8ab
Karis Caruso <cchristophercmc@gmail.com>
2024-08-22 17:31:19 -0400

    Update inOrderEqual.java

--- java/Logic-1/inOrderEqual.java
+++ java/Logic-1/inOrderEqual.java
@@ -1,4 +1,6 @@
 public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
-  if(equalOk) return a <= b && b <= c;
-  else return a < b && b < c;
+    if(equalOk) {
+        return a <= b && b <= c;
+    }
+    return a < b && b < c;
 }
