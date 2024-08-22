c02ee9c72207a1c88d796e21118d409e1000f40e
Karis Caruso <cchristophercmc@gmail.com>
2024-08-22 17:21:01 -0400

    Update fizzString2.java

--- java/Logic-1/fizzString2.java
+++ java/Logic-1/fizzString2.java
@@ -1,7 +1,9 @@
 public String fizzString2(int n) {
     String ret = "";
+
     ret += (n % 3 == 0)? "Fizz" : "";
     ret += (n % 5 == 0)? "Buzz" : "";
+
     if(ret.equals("")) {
         return n + "!";
     }
