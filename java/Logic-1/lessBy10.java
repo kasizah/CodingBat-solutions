b1c1d01ffdf453e1e9aa1e141ae83b16b8eb518e
Karis Caruso <cchristophercmc@gmail.com>
2024-08-23 16:34:18 -0400

    Update lessBy10.java

--- java/Logic-1/lessBy10.java
+++ java/Logic-1/lessBy10.java
@@ -1,5 +1,5 @@
 public boolean lessBy10(int a, int b, int c) {
-  return (Math.abs(a - b) >= 10 ||
-          Math.abs(b - c) >= 10 ||
-          Math.abs(c - a) >= 10);
+    return (Math.abs(a - b) >= 10 ||
+            Math.abs(b - c) >= 10 ||
+            Math.abs(c - a) >= 10);
 }
