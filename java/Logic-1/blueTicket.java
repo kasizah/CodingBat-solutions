978cc8c0e1351e03ca43a742b4c5ae5e2b43ab53
Karis Caruso <cchristophercmc@gmail.com>
2024-08-26 16:50:39 -0400

    Update blueTicket.java

--- java/Logic-1/blueTicket.java
+++ java/Logic-1/blueTicket.java
@@ -1,6 +1,12 @@
 public int blueTicket(int a, int b, int c) {
-  int ab = a + b, bc = b + c, ac = a + c;
-  if(ab == 10 || bc == 10 || ac == 10) return 10;
-  else if(ab == bc + 10 || ab == ac + 10) return 5;
-  else return 0;
+    int ab = a + b,
+        bc = b + c,
+        ac = a + c;
+    if(ab == 10 || bc == 10 || ac == 10) {
+        return 10;
+    } else if(ab - bc == 10 || ab - ac == 10) {
+        return 5;
+    } else {
+        return 0;
+    }
 }
