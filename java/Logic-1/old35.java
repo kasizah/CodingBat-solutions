6be6cbee56eb3c58ec974bf483b7978840728f86
Karis Caruso <cchristophercmc@gmail.com>
2024-08-26 16:56:22 -0400

    Update old35.java

--- java/Logic-1/old35.java
+++ java/Logic-1/old35.java
@@ -1,4 +1,4 @@
 public boolean old35(int n) {
-  if(n % 3 == 0 && n % 5 == 0) return false;
-  else return n % 3 == 0 || n % 5 == 0;
+    return (n % 3 == 0 && n % 5 != 0) 
+        || (n % 3 != 0 && n % 5 == 0);
 }
