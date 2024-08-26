09341337130ac17b253fd0dd714fed75b8cc9507
Karis Caruso <cchristophercmc@gmail.com>
2024-08-26 16:54:01 -0400

    Update maxMod5.java

--- java/Logic-1/maxMod5.java
+++ java/Logic-1/maxMod5.java
@@ -1,8 +1,7 @@
 public int maxMod5(int a, int b) {
-  if(a == b) return 0;
-  else if(a % 5 == b % 5) {
-    return (a < b)? a : b;
-  } else {
+    if(a == b) return 0;
+    if(a % 5 == b % 5) {
+        return (a > b)? b : a;
+    }
     return (a > b)? a : b;
-  }
 }
