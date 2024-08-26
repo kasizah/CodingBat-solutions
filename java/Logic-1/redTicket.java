7e970ce4eb44bb3255883cb02010368629bdb67c
Karis Caruso <cchristophercmc@gmail.com>
2024-08-26 16:57:38 -0400

    Update redTicket.java

--- java/Logic-1/redTicket.java
+++ java/Logic-1/redTicket.java
@@ -1,6 +1,6 @@
 public int redTicket(int a, int b, int c) {
-  if(a == b && b == c && c == 2) return 10;
-  else if(a == b && b == c) return 5;
-  else if(a != b && a != c) return 1;
-  else return 0;
+    if(a == b && b == c) {
+        return (a == 2)? 10 : 5;
+    }
+    return (a != b && a != c)? 1 : 0;
 }
